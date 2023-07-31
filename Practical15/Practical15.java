cd22601926f3d012fd7b12040c31b6182b8ba1fb
Isurikaherath <140747574+Isurikaherath@users.noreply.github.com>
2023-07-31 20:44:43 +0530

    Add files via upload

---
+++ Practical15/Practical15.java
@@ -0,0 +1,23 @@
+package com.mycompany.practical15;
+public class Practical15 
+{
+    public static void main(String[] args)
+    {
+       	char grade ='F';
+	 switch(grade)
+	{
+	case 'A' :
+	System.out.println("Excellent!"); 
+	break;
+	case 'D' :
+	System.out.println("You passed");
+	case 'F' :
+	System.out.println("Better try again");
+	break;
+	default :
+	System.out.println("Invalid grade");
+	}
+	System.out.println("Your grade is " + grade);
+
+    }
+}
