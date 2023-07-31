a23c8f43e63e2057aab0eab33db4218fc242c6e0
Isurikaherath <140747574+Isurikaherath@users.noreply.github.com>
2023-07-31 20:05:20 +0530

    Add files via upload

---
+++ practical 1/Que2.java
@@ -0,0 +1,9 @@
+package com.exepmycompany.que2;
+public class Que2
+{
+    public static void main(String[] args)
+    {
+        System.out.println("udari herath");
+        System.out.println("BSc (hons) in Software Engineering");
+    }
+}
